aceeacddf71637f7f2b3abb648584bf5f61aae6d
Bruce Harrison <bruce.harrison@hashicorp.com>
2024-09-10 15:11:30 -0500

    wip

--- tests/setup/main.tf
+++ tests/setup/main.tf
@@ -7,7 +7,7 @@ variable "crypto_key" {
 }
 
 ## key_rings cannot be deleted, so we'll have to use a stationary one named 'tf-integration-test'
-## this is to prevent creating a new key_ring for each test
+## this is to prevent creating a new key_ring for each test.
 data "google_kms_key_ring" "setup" {
   name     = "tf-integration-test"
   location = var.location
